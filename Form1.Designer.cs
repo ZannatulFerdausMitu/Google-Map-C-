b9d168641f37bfb20f39d25a4d426e01525fedcd
ZannatulFerdausMitu <89478485+ZannatulFerdausMitu@users.noreply.github.com>
2021-09-14 14:00:07 -0700

    Add files via upload
    Google Map some files added

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace GoogleMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Search_data = new System.Windows.Forms.Button();
+            this.txt_zip = new System.Windows.Forms.TextBox();
+            this.txt_state = new System.Windows.Forms.TextBox();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.txt_street = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Black;
+            this.splitContainer1.Panel1.Controls.Add(this.Search_data);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_zip);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_state);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_city);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_street);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(680, 483);
+            this.splitContainer1.SplitterDistance = 232;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Search_data
+            // 
+            this.Search_data.BackColor = System.Drawing.Color.White;
+            this.Search_data.BackgroundImage = global::GoogleMap.Properties.Resources.istockphoto_847709466_612x612;
+            this.Search_data.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Search_data.FlatAppearance.BorderSize = 0;
+            this.Search_data.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Search_data.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search_data.ForeColor = System.Drawing.Color.Yellow;
+            this.Search_data.Location = new System.Drawing.Point(48, 264);
+            this.Search_data.Name = "Search_data";
+            this.Search_data.Size = new System.Drawing.Size(122, 46);
+            this.Search_data.TabIndex = 8;
+            this.Search_data.Text = "Search";
+            this.Search_data.UseVisualStyleBackColor = false;
+            this.Search_data.Click += new System.EventHandler(this.Search_data_Click);
+            // 
+            // txt_zip
+            // 
+            this.txt_zip.BackColor = System.Drawing.Color.LightSalmon;
+            this.txt_zip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_zip.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold);
+            this.txt_zip.ForeColor = System.Drawing.Color.Black;
+            this.txt_zip.Location = new System.Drawing.Point(81, 186);
+            this.txt_zip.Name = "txt_zip";
+            this.txt_zip.Size = new System.Drawing.Size(119, 25);
+            this.txt_zip.TabIndex = 7;
+            // 
+            // txt_state
+            // 
+            this.txt_state.BackColor = System.Drawing.Color.LightSalmon;
+            this.txt_state.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_state.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold);
+            this.txt_state.ForeColor = System.Drawing.Color.Black;
+            this.txt_state.Location = new System.Drawing.Point(81, 143);
+            this.txt_state.Name = "txt_state";
+            this.txt_state.Size = new System.Drawing.Size(119, 25);
+            this.txt_state.TabIndex = 6;
+            // 
+            // txt_city
+            // 
+            this.txt_city.BackColor = System.Drawing.Color.LightSalmon;
+            this.txt_city.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_city.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold);
+            this.txt_city.ForeColor = System.Drawing.Color.Black;
+            this.txt_city.Location = new System.Drawing.Point(81, 105);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(119, 25);
+            this.txt_city.TabIndex = 5;
+            // 
+            // txt_street
+            // 
+            this.txt_street.BackColor = System.Drawing.Color.LightSalmon;
+            this.txt_street.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_street.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_street.ForeColor = System.Drawing.Color.Black;
+            this.txt_street.Location = new System.Drawing.Point(81, 62);
+            this.txt_street.Name = "txt_street";
+            this.txt_street.Size = new System.Drawing.Size(119, 25);
+            this.txt_street.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label4.Location = new System.Drawing.Point(12, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Zip";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "State";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "City";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Street";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(444, 483);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(680, 483);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Google Map";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button Search_data;
+        private System.Windows.Forms.TextBox txt_zip;
+        private System.Windows.Forms.TextBox txt_state;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.TextBox txt_street;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
